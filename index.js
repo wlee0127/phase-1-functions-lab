f6792872ea293d0987e9b2e526ed6f937d24f7bc
william lee <will.lee.npc@gmail.com>
2023-09-18 02:19:43 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(arg) {
+    return Math.abs(arg-42);
+}
+
+function distanceFromHqInFeet(arg) {
+    let blocks = Math.abs(arg-42);
+    return blocks*264;
+
+}
+
+function distanceTravelledInFeet(arg1, arg2) {
+    let blocks = Math.abs(arg1-arg2);
+    return blocks*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let blocks = distanceTravelledInFeet(start, destination);
+    
+    return blocks <= 400 ? 0
+    : blocks>400 && blocks<=2000 ? (blocks-400)*0.02
+    : blocks>2000 && blocks<=2500 ? 25
+    : "cannot travel that far";
+
+}
+
